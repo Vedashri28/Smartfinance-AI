4bd198cbff98344c5601d337df425a91d22d596f
Vedashri Giri <girivedashri2015@gmail.com>
2026-07-13 23:44:18 +0530

    feat(backend): initialize Express backend structure

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "🚀 SmartFinance AI Backend Running Successfully!"
+  });
+});
+
+module.exports = app;
